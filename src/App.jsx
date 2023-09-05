b6149f6eb75ca2bb09ddd350f658ffc04488f6f4
ure <uy23@naver.com>
2023-09-05 23:51:20 +0900

    chore #1: emotion 설치

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import styled from "@emotion/styled";
 
 function App() {
+  const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: skyblue;
+    height: 100vh;
+  `;
 
-  return (
-    <>
-      empty
-    </>
-  )
+  return <Container>empty</Container>;
 }
 
-export default App
+export default App;
